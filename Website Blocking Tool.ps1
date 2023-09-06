469e8fb50dfa8452789e6719078f042a4fc1c33d
JoshWigner <josh.wigner@fpcr.co.uk>
2023-09-06 15:10:49 +0100

    Create Website Blocking Tool.ps1

---
+++ Website Blocking Tool.ps1
@@ -0,0 +1,19 @@
+﻿function Format-Domain {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$domain
+    )
+    
+    # Strip off 'http://' or 'https://' if provided
+    $domain = $domain -replace "^http://", "" -replace "^https://", ""
+
+    # Output the different formats
+    "*.$domain/*"
+    "*.$domain"
+    "  $domain/*"
+    "  $domain "
+}
+
+# Test the function
+$domainInput = Read-Host -Prompt 'Enter the domain'
+Format-Domain -domain $domainInput
